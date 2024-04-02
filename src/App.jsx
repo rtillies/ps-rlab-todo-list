d2cae6afa24fe22d0a16c6aa451b2b1dc221b538
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-02 10:42:28 -0400

    UseReducer for increment

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useReducer } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function reducer(state, action) {
+  return {count: state.count + 1}
+}
+
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, {count: 0})
+  // const [count, setCount] = useState(0)
 
   function increment() {
-    setCount(prevCount => prevCount + 1)
+    // setCount(prevCount => prevCount + 1)
+    dispatch()
   }
   function decrement() {
-    setCount(prevCount => prevCount - 1)
+    // setCount(prevCount => prevCount - 1)
   }
 
   return (
     <>
       <div className='App'>
         <button onClick={decrement}>-</button>
-        <span className='count'> {count} </span>
+        <span className='count'> {state.count} </span>
         <button onClick={increment}>+</button>
       </div>
     </>
   )
 }
 
-export default App
+// export default App
