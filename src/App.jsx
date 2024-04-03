371be335da84102ccda7461af14c67780ec80de6
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-03 13:04:55 -0400

    reducer works, show todo list with buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import React, { useState, useReducer } from 'react'
-import './App.css'
+import React, { useState, useReducer } from "react";
+import Todo from "./Todo";
+import "./App.css";
 
 const ACTIONS = {
-  ADD_TODO: 'add-todo'
+  ADD_TODO: "add-todo",
+  TOGGLE_TODO: "toggle-todo",
+};
+
+function reducer(todos, action) {
+  switch (action.type) {
+    case ACTIONS.ADD_TODO:
+      return [...todos, newTodo(action.payload.name)];
+    case ACTIONS.TOGGLE_TODO:
+      return 
+    default:
+      return todos;
+  }
 }
 
-function reducer(state, action) {
-
+function newTodo(name) {
+  return { id: Date.now(), name: name, complete: false };
 }
 
 export default function App() {
-  const [todos, dispatch] = useReducer(reducer, [])
-  const [name, setName] = useState('')
+  const [todos, dispatch] = useReducer(reducer, []);
+  const [name, setName] = useState('');
 
-  function handleSubmit() {
+  function handleSubmit(e) {
     e.preventDefault();
-    dispatch({type: ACTIONS.ADD_TODO})
-    setName('')
+    dispatch({ 
+      type: ACTIONS.ADD_TODO,
+      payload: { name: name }
+    });
+    setName('');
   }
+
+  console.log(todos);
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          value={name} 
-          onChange={e => setName(e.target.value)}
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
       </form>
+      {todos.map(todo => {
+        return <Todo key={todo.id} todo={todo} />
+      })}
     </>
-  )
+  );
 }
