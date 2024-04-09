4a39f1e6a4e35066b573c5bf15888297b18643df
Richard T <74796181+rtillies@users.noreply.github.com>
2024-04-09 08:13:14 -0400

    1 row, 2 columns

--- src/App.jsx
+++ src/App.jsx
@@ -69,8 +69,9 @@ export default function App() {
 
   return (
     <>
-      <div className="col-md-4 col-sm-6">
-        <form onSubmit={handleSubmit}>
+    <div className="row">
+      <div className="col-sm-6">
+      <form onSubmit={handleSubmit}>
           <h1>Todo List</h1>
           <h2>Create New Item</h2>
           <input
@@ -81,11 +82,15 @@ export default function App() {
           />
           <button>Add</button>
         </form>
+      </div>
+
+      <div className="col-sm-6">
         {/* <h3>Todo Items</h3> */}
         {todos.map((todo) => {
           return <Todo key={todo.id} todo={todo} dispatch={dispatch} />;
         })}
       </div>
+    </div>
     </>
   );
 }
